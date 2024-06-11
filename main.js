8412f00cb4e5d32def859dd852cc8839bfd90ff9
andre <108833211+andredomareski@users.noreply.github.com>
2024-06-11 11:20:31 -0300

    1106

--- main.js
+++ main.js
@@ -64,4 +64,4 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
 }
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = "um computador pode levar até" + Math.floor(2**entropia)/(100e6*60*60*24) + "dias para descobrir essa senha";
